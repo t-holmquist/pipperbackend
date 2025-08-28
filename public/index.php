ef6b3727296744f57052718ca34f3b6d5515221f
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-08-28 21:44:17 +0200

    Add connection to mysql db

--- public/index.php
+++ public/index.php
@@ -10,8 +10,23 @@
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, XRequested-With");
 
-    
+    // Connect to the MySQL server with the pips.
+    // Handles the error in case of failed connection
+    $servername = "localhost";
+    $username = "root";
+    $password = getenv('PASSWORD');
 
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=Pipper", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+    
 
     
 
